bf38a95506ec28b4af8025cfb119794395b691a7
Laurin49 <welter@diwe.info>
2023-01-29 18:22:03 +0100

    CH05 Layout View

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,12 @@
+<div class="copyright py-4 text-center text-white">
+    <div class="container">
+        <small>
+            Copyright - <a class="text-reset fw-bold text-decoration-none" target="_blank"
+                           href="http://www.welter@diwe-dev.de">
+                Dirk Welter
+            </a> - <b>DIWE-EDV</b>
+        </small>
+    </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
+</script>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,6 @@
+
+<header class="masthead bg-primary text-white text-center py-4">
+    <div class="container d-flex align-items-center flex-column">
+        <h2>@yield('subtitle', 'A Laravel Online Store')</h2>
+    </div>
+</header>
